b4f9f6527b467a71eb42943f03862b3424926c49
xKit-22 <78998441+xKit-22@users.noreply.github.com>
2022-05-23 19:52:11 +0400

    commit

---
+++ entities/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+   id?: string
+   nickname: string
+   avatar: string
+   postsAmount: number
+   subscribersAmount: number
+   subscriptionsAmount: number
+   allLikesAmount: number
+   dateOfCreation: string
+   userLogin: string
+   userPassword: string
+}
